f70564efe378a6ca804519c5ea20ff3617f884b2
Vincent Delacourt <vincent.delacourt@adama-development.com>
2019-01-16 23:10:43 +0800

    rewrite type react-native mob

--- src/types/react-native-admob.d.ts
+++ src/types/react-native-admob.d.ts
@@ -1,9 +1,10 @@
+/* tslint:disable */
 declare module "react-native-admob" {
   export class AdMobInterstitial {
-    static requestAd: (any?: any) => any;
-    static showAd: (any?: any) => any;
-    static setTestDevices: (any?: any) => any;
-    static setAdUnitID: (any?: any) => any;
+    static requestAd: () => Promise<void>;
+    static showAd: () => void;
+    static setTestDevices: (testDevices?: string[]) => void;
+    static setAdUnitID: (unitId: string) => void;
     static simulatorId: string;
   }
 }
